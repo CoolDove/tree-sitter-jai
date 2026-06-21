6e9293237a9dde5c4148b66d56c4a0bdf2ac2d9d
Dove <1515098414@qq.com>
2026-06-22 04:31:47 +0800

    fix highlights.scm

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,269 +1,63 @@
-; Includes
-
 [
-  (import)
-  (load)
-] @include
-
-
-; Keywords
-[
-  ; from modules/Jai_Lexer
   "if"
-  "xx"
-
-  "ifx"
-  "for"
-
-  "then"
   "else"
-  "null"
-  "case"
-  "enum"
-  "true"
-  "cast"
-
+  "for"
   "while"
+  "return"
   "break"
-  "using"
+  "continue"
   "defer"
-  "false"
-  "union"
-
-  "return"
-  "struct"
-  "inline"
   "remove"
-
-  ; "size_of"
-  "type_of"
-  ; "code_of"
-  ; "context"
-
-  "continue"
-  "operator"
-
-  ; "type_info"
-  "no_inline"
-  "interface"
-
-  "enum_flags"
-
-  ; "is_constant"
-
-  "push_context"
-
-  ; "initializer_of"
 ] @keyword
-
-[
-  "return"
-] @keyword.return
-
-[
-  "if"
-  "else"
-  "case"
-  "break"
-] @conditional
-
-((if_expression
-  [
-    "then"
-    "ifx"
-    "else"
-  ] @conditional.ternary)
-  (#set! "priority" 105))
-
-; Repeats
-
-[
-  "for"
-  "while"
-  "continue"
-] @repeat
-
-; Variables
-
-; (identifier) @variable
-name: (identifier) @variable
-argument: (identifier) @variable
-named_argument: (identifier) @variable
-(member_expression (identifier) @variable)
-(parenthesized_expression (identifier) @variable)
-
-((identifier) @variable.builtin
-  (#any-of? @variable.builtin "context"))
-
-; Namespaces
-
-(import (identifier) @namespace)
-
-; Parameters
-
-(parameter (identifier) @parameter ":" "="? (identifier)? @constant)
-
-; (call_expression argument: (identifier) @parameter "=")
-
-; Functions
-
-; (procedure_declaration (identifier) @function (procedure (block)))
-(procedure_declaration (identifier) @function (block))
-
-(call_expression function: (identifier) @function.call)
-
-; Types
-
-type: (types) @type
-type: (identifier) @type
-((types) @type)
-
-modifier: (identifier) @keyword
-keyword: (identifier) @keyword
-
-((types (identifier) @type.builtin)
-  (#any-of? @type.builtin
-    "bool" "int" "string"
-    "s8" "s16" "s32" "s64"
-    "u8" "u16" "u32" "u64"
-    "Type" "Any"))
-
-(struct_declaration (identifier) @type ":" ":")
-
-(enum_declaration (identifier) @type ":" ":")
-
-; (const_declaration (identifier) @type ":" ":" [(array_type) (pointer_type)])
-
-; ; I don't like this
-; ((identifier) @type
-;   (#lua-match? @type "^[A-Z][a-zA-Z0-9]*$")
-;   (#not-has-parent? @type parameter procedure_declaration call_expression))
-
-; Fields
-
-(member_expression "." (identifier) @field)
-
-(assignment_statement (identifier) @field "="?)
-(update_statement (identifier) @field)
-
-; Constants
-
-((identifier) @constant
-  (#lua-match? @constant "^_*[A-Z][A-Z0-9_]*$")
-  (#not-has-parent? @constant type parameter))
-
-(member_expression . "." (identifier) @constant)
-
-(enum_field (identifier) @constant)
-
-; Literals
-
-(integer) @number
-(float) @number
 
 (string) @string
 
-;(character) @character
+(compiler_directive) @keyword.directive
+"#type" @keyword.directive
 
-(string_contents (escape_sequence) @string.escape)
+(types) @type
 
-(boolean) @boolean
+(struct_declaration
+  name: (identifier) @type
+)
+
+(named_parameters
+  parameters: (parameter) @variable
+)
 
 [
-  (uninitialized)
-  (null)
-] @constant.builtin
-
-; Operators
-
-[
-  ":"
-  "="
-  "+"
-  "-"
-  "*"
-  "/"
-  "%"
-  ">"
-  ">="
-  "<"
-  "<="
-  "=="
-  "!="
-  "|"
-  "~"
-  "&"
-  "&~"
-  "<<"
-  ">>"
-  "<<<"
-  ">>>"
-  "||"
-  "&&"
-  "!"
-  ".."
-  "+="
-  "-="
-  "*="
-  "/="
-  "%="
-  "&="
-  "|="
-  "^="
-  "<<="
-  ">>="
-  "<<<="
-  ">>>="
-  "||="
-  "&&="
+ "+"
+ "-"
+ "*"
+ "/"
+ "%"
+ "=="
+ "!="
+ "<"
+ "<="
+ ">"
+ ">="
+ "&&"
+ "||"
+ "!"
 ] @operator
 
-; Punctuation
+(call_expression
+  function: (identifier) @variable
+) @function.call
 
-[ "{" "}" ] @punctuation.bracket
 
-[ "(" ")" ] @punctuation.bracket
+(const_declaration
+  name: (identifier) @constant
+)
+(enum_declaration
+  name: (identifier) @constant
+)
 
-[ "[" "]" ] @punctuation.bracket
+(struct_literal
+  name: (identifier) @variable
+)
 
-[
-  "`"
-  "->"
-  "."
-  ","
-  ":"
-  ";"
-] @punctuation.delimiter
 
-; Comments
-
-[
-  (block_comment)
-  (comment)
-] @spell
-
-[
-  (block_comment)
-  (comment)
-] @comment 
-
-; Tagged unions
-
-(tagged_union_kind name: (identifier) @keyword)
-
-(tagged_union_binding tag: (identifier) @constant)
-
-",," @operator
-
-; Errors
-
-(ERROR) @error
-
-directive: ("#") @keyword ; #if
-type: ("type_of") @type
-
-(compiler_directive) @keyword
-(heredoc_start) @none
-(heredoc_end) @none
-(heredoc_body) @string
-(note) @string
+(comment) @comment
+(block_comment (block_comment_text) @comment)
